030a8747014bfbd93052ed3ae2b629ea09a99ead
S. Hutchins <ms.s.hutchins@gmail.com>
2019-04-25 19:19:58 -0500

    Remove bin folder.

--- bin/git_status_checker_ignore_untracked.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM Change to projects root directory:
-cd %~dp0\..\..
-
-REM Invoke git_status_checker script from projects root directory:
-python %~dp0\..\git_status_checker\git_status_checker.py --ignore-untracked %* .
-
-IF ERRORLEVEL 1 pause
